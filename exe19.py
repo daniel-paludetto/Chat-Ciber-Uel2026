6bf83ce9ede2720a490cf27d3fb70a424b7dfc1b
Daniel Paludetto <geornal.daniel@gmail.com>
2026-05-06 22:48:37 -0300

    Add files via upload

---
+++ exe19.py
@@ -0,0 +1,13 @@
+raio = float(input('informe seu raio\n'))
+
+pi = 3.1415
+
+c = 2*pi*raio
+
+v = (4/3)*pi*raio**3
+
+ac = pi*raio**2
+
+ass = 4*pi*raio**2
+
+print(f"comprimeto: {c}\nvolume: {v}\narea do circulo: {ac}\narea superficial: {ass}")
